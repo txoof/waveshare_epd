88a5e3bfdc2fe94158da70abeb71c39c95ea028c
txoof <aaron.ciuffo@gmail.com>
2021-04-14 20:19:47 +0200

    remove time.sleep

--- lib/waveshare_epd/epdconfig.py
+++ lib/waveshare_epd/epdconfig.py
@@ -1,31 +1,12 @@
-# /*****************************************************************************
-# * | File        :	  epdconfig.py
-# * | Author      :   Waveshare team
-# * | Function    :   Hardware underlying interface
-# * | Info        :
-# *----------------
-# * | This version:   V1.0
-# * | Date        :   2019-06-21
-# * | Info        :   
-# ******************************************************************************
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documnetation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to  whom the Software is
-# furished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS OR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-#
+#!/usr/bin/env python3
+# coding: utf-8
+
+
+
+
+
+
+
 
 import os
 import logging
@@ -54,7 +35,10 @@ class RaspberryPi:
         return self.GPIO.input(pin)
 
     def delay_ms(self, delaytime):
-        time.sleep(delaytime / 1000.0)
+        cur_time = time.time()
+        while time.time() < cur_time + delaytime/1000:
+            pass
+#         time.sleep(delaytime / 1000.0)
 
     def spi_writebyte(self, data):
         self.SPI.writebytes(data)
@@ -156,3 +140,12 @@ for func in [x for x in dir(implementation) if not x.startswith('_')]:
 
 
 ### END OF FILE ###
+
+
+
+
+
+
+
+
+
